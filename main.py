6e98ed511727876a4e92701f8f96f790bb93a043
Amper <andrey.maliutin@gmail.com>
2023-03-02 22:52:34 +0300

    получение списка файлов из папки

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-import sys
+import sys, os
 import win32gui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon, QPixmap
 
 
-# Это окно, которое всегда находится сверху, не имеет границ и всегда имеет ту же ширину, что и панель задач.
 class Sidebar(QMainWindow):
-    """Класс боковой панели"""
-
+    """Класс боковой панели.
+    :param left : Левая или правая панель.
+    :type left : bool
+    :param coord_y : Координата отступа сверху. По умолчанию 0.
+    :type coord_y : int
+    :param width : Ширина панели.
+    :type width : int
+    """
     def __init__(self,
                  left: bool,
                  coord_y=0,
@@ -17,9 +23,10 @@ class Sidebar(QMainWindow):
                  ):
         super().__init__()
 
+        self.file_list = []
+
         self.desktop = QApplication.desktop().screenGeometry()
         self.desktop_width = self.desktop.width()
-        self.desktop_height = self.desktop.height()
         width = int(self.desktop_width / 10)
         taskbar_hwnd = win32gui.FindWindow("Shell_TrayWnd", None)
         height = win32gui.GetWindowRect(taskbar_hwnd)[1]
@@ -29,19 +36,32 @@ class Sidebar(QMainWindow):
         else:
             coord_x = self.desktop_width - width
 
+        button = QPushButton("Кнопа", self)
+        button.move(20, 20)
+
         self.setWindowFlags(Qt.FramelessWindowHint)
         self.setGeometry(coord_x, coord_y, width, height)
 
-        label1 = QLabel(str(int(self.desktop_width / 10)), self)
-        label1.move(50, 50)
+    def get_icon(self):
+        path = "C:/Users/user/Desktop/Games"
+        icon = QIcon(path)
+        print(icon)
+        pixmap = icon.pixmap(QSize(64, 64))
+
+        pixmap.save("C:/qqqq.png", "PNG")
+
+    def get_files(self):
+        path = "C:/Users/user/Desktop/Games"
+        self.file_list = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+        print(self.file_list)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    print()
 
     window1 = Sidebar(True)
     window1.show()
+    window1.get_files()
 
     window2 = Sidebar(False)
     window2.show()
